407e97fc2fa9fd4f693c29ace9932b80a1aa7138
arjun2020-py <arjunkoodathil@gmail.com>
2022-12-21 20:58:04 +0530

    make some changes

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -28,7 +28,7 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const SplashScreen(),
+      home: LoginPage(),
       routes: {
         'Login': (context) => LoginPage(),
         // 'Siginup':(context) => SiginupPage(),
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -6,12 +6,23 @@
 // https://opensource.org/licenses/MIT.
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/app/app.dart';
 import 'package:todo/bootstrap.dart';
 
+//1 firebase messaging
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print(message.messageId);
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+//2
+  await FirebaseMessaging.instance
+      .requestPermission(alert: true, sound: true, badge: true);
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   bootstrap(() => const App());
 }
